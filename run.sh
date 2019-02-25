5943f824ce0d4f1c315248729450701e7102ddc0
Terence <terence@192-168-1-4.tpgi.com.au>
2019-02-25 21:31:32 +1100

    improved run script

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source ./_vars.sh
 # ================================================================
 # Command-line Assistant - run.sh
 # - Neo4j
@@ -6,89 +7,96 @@
 
 
 echo =============================================================
-echo Hi $USER@$HOSTNAME. 
-echo What do you want to do?
+echo run.sh -a [action] -b [label] -c [container]
 echo -------------------------------------------------------------
-echo 01 : Docker: Build
-echo 02 : Docker: Run Container
-echo 03 : Docker: Stop Container
-echo 04 : Docker: View Logs 
-echo ----------------------------------------------
-echo qq : Exit [Quit]
-echo Enter [Selection] to continue
+echo -a 01 : Docker: Build
+echo -a 02 : Docker: Run Container
+echo -a 03 : Docker: Stop Container
+echo -a 04 : Docker: Remove all Containers
+echo -a 05 : Docker: View Logs 
 echo =============================================================
-# Command line selection
-if [ -n "$1" ]; then
-  SELECTION=$1
-else
-  read  -n 2 SELECTION
-fi
-if [ -n "$2" ]; then
-  PARAM2=$2
-else
-  read  -n  PARAM2
-fi
-echo Your selection is : $SELECTION.
-echo Your parameter is : $PARAM2.
 
-IMAGE=myneo4j
+#Defaults
+#ACTION="up"
+LABEL="latest"
 CONTAINER=myneo4jc
+IMAGE=myneo4j
+TARGET=prod
 
-case "$SELECTION" in
-# Note variable is quoted.
+while getopts "a:b:c:t:" opt; do
+  case "${opt}" in 
+    "a" )
+      ACTION="${OPTARG}"
+      echo "Action:" $ACTION
+      ;;
+    "b" )
+      LABEL="${OPTARG}"
+      echo "Label:" $LABEL
+      ;;
 
+    "c" )
+      CONTAINER="${OPTARG}"
+      echo "Container:" $CONTAINER
+      ;;
+
+    "t" )
+      TARGET="${OPTARG}"
+      echo "Target:" $TARGET
+      ;;
+  esac
+done
+
+
+case "$ACTION" in
   "01" )
-  echo ===========================================================
-  echo Docker Build
-  echo ===========================================================
-  docker build -t $IMAGE .
-  ;;
+    echo ===========================================================
+    echo Docker Build
+    echo ===========================================================
+    docker build -t $IMAGE .
+    ;;
 
   "02" )
-  echo ===========================================================
-  echo Docker Run
-  echo ===========================================================
-  docker run -d \
+    echo ===========================================================
+    echo Docker Run
+    echo ===========================================================
+    docker run -d \
     --publish=7474:7474 --publish=7687:7687 \
     --volume=$HOME/neo4j/data:/data \
     --volume=$HOME/neo4j/logs:/logs \
     $IMAGE \
 #    --name $CONTAINER \
-
-#neo4j:3.0
-
-  ;;
-
+    ;;
 
   "03" )
-  echo ===========================================================
-  echo Docker Stop
-  echo ===========================================================
-  docker container stop $CONTAINER
-  ;;
-
-
+    echo ===========================================================
+    echo Docker Stop
+    echo ===========================================================
+    docker container stop $CONTAINER
+    ;;
 
   "04" )
-  echo ===========================================================
-  echo Docker Logs
-  echo ===========================================================
-  docker logs $CONTAINER
-  ;;
+    echo ===========================================================
+    echo Docker Remove Containers
+    echo ===========================================================
+    docker stop $(docker ps -aq)
+    ;;
 
+  "05" )
+    echo ===========================================================
+    echo Docker Logs
+    echo ===========================================================
+    docker logs -f $CONTAINER
+    ;;
 
-  "qq" )
-  echo Quit
-  exit 0
-  ;;
-
-   * )
-   # Default option.
-   # Empty input (hitting RETURN) fits here, too.
-   echo
-   echo "Not a recognized option."
-  ;;
-
+  * )
+    # Default option.
+    # Empty input (hitting RETURN) fits here, too.
+    echo
+    echo "Not a recognized option."
+    ;;
 esac
+
+
+
 
 
